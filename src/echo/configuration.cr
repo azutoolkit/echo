015d6df6db50d8e695894d5d33c2320d74768adb
Elias J. Perez <eliasjpr@gmail.com>
2020-10-17 11:02:23 -0400

    Authenticates redis

--- src/echo/configuration.cr
+++ src/echo/configuration.cr
@@ -1,8 +1,14 @@
 module Echo
   class Configuration
     property websoket_uri = "ws://localhost:4000/hi"
-    property redis : MiniRedis = MiniRedis.new(
-      uri: URI.parse(ENV["REDIS_URL"])
-    )
+    property redis : MiniRedis
+
+    def initialize
+      @redis = MiniRedis.new uri: URI.parse(redis_url)
+    end
+
+    def redis_url
+      ENV.fetch "REDIS_URL", "redis://localhost:6379"
+    end
   end
 end
